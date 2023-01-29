b6e435e09c61458527f769f52f47b064543c042f
Veralee225 <Veralee225@gmail.com>
2023-01-29 09:48:20 +0100

    chore: dependencies upgrade and code

---
+++ food-community-project/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                Veralee's foodary &copy; 2023
+            </footer>
+        </div>
+    );
+}
+export default Footer;
---
+++ food-community-project/src/recipe.js
@@ -0,0 +1,38 @@
+export const my_recipe = {
+    recipe_1: {
+        name: 'Chirashi Don',
+        preparation_time: '30 minutes',
+        servings: '2',
+        ingredients: [
+            '1/2 cup rice',
+            '1/2 cup water',
+            '2 eggs',
+            '300g of salmon, tuna and salmon roe',
+        ],
+        instructions: [
+            'Cook rice and water in a rice cooker',
+            'Fry eggs in a pan',
+            'Serve rice, eggs and fish on a plate',
+        ],
+
+    },
+    recipe_2: {
+        name: "It's Chilli by George",
+        preparation_time: '10 minutes',
+        servings: '10',
+        ingredients: [
+            '2 pounds lean ground beef',
+            '1 (46 fluid ounce) can tomato juice',
+            '1 (29 ounce) can tomato sauce',
+            '1 (15 ounce) can kidney beans, drained and rinsed',
+            '1 (15 ounce) can pinto beans, drained and rinsed',
+            '1 ½ cups chopped onion'
+        ],
+        instructions: [
+            'Heat a large skillet over medium-high heat. Cook and stir ground beef in the hot skillet until browned and crumbly, 5 to 7 minutes. Drain and discard grease.',
+            'Transfer browned beef into a large pot over medium heat; stir in tomato juice, tomato sauce, kidney beans, pinto beans, onions, bell pepper, chili powder, cumin, salt, sugar, oregano, ground black pepper, and cayenne pepper. Bring to a boil; reduce heat to low and simmer for 1 1/2 hours.',
+            'Step 3'
+        ],
+
+    }
+}
